0953c17c4406d2f64f6a4547003d29d7614fa320
yassine <elbayass59@gmail.com>
2018-04-11 11:45:30 +0200

    site complet

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+define('URL', 'http://duythien.dev/sitepoint/blog-mongodb');
+define('UserAuth', 'root');
+define('PasswordAuth', 'simplonco');
+
+$config = array(
+	'username' => 'root',
+	'password' => 'simplonco',
+	'dbname'   => 'blog',
+	//'cn' 	   => sprintf('mongodb://%s:%d/%s', $hosts, $port,$database),
+	'connection_string'=> sprintf('mongodb://%s:%d/%s','127.0.0.1','27017','blog')
+);
